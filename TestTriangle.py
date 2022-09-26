a672e3a74f0e83ea137221928eaf4140bcd22051
RK-ops <63204698+RK-ops@users.noreply.github.com>
2022-09-26 14:23:23 -0400

    Update TestTriangle.py

--- TestTriangle.py
+++ TestTriangle.py
@@ -21,35 +21,17 @@ class TestTriangles(unittest.TestCase):
     def testEquilateralTriangles2(self):
         self.assertEqual(classifyTriangle(7, 7, 7), 'Equilateral', '7,7,7 should be equilateral trinagle')
 
-    def testEquilateralTriangles3(self):
-        self.assertEqual(classifyTriangle(15, 1, 15), 'Equilateral', '15,1,15 should be equilateral trinagle')
-
     def testRightTriangle1(self):
         self.assertEqual(classifyTriangle(3, 4, 5),'Right', '3,4,5 should be Right triangle')
-
-    def testRightTriangle2(self):
-        self.assertEqual(classifyTriangle(5, 3, 4), 'Right', '5,3,4 should be Right triangle')
-
-    def testRightTriangle3(self):
-        self.assertEqual(classifyTriangle(13, 12, 5), 'Right', '13,12,5 should be Right triangle')
-
+        
     def testRightTriangle4(self):
         self.assertEqual(classifyTriangle(8, 6, 10), 'Right', '8,6,10 should be Right triangle')
-
-    def testRightTriangle5(self):
-        self.assertEqual(classifyTriangle(21, 6, 10), 'Right', '21,6,10 should be Right triangle')
 
     def testScaleneTriangle1(self):
         self.assertEqual(classifyTriangle(10, 11, 12),'Scalene','10,11,12 should be Scalene triangle')
 
-    def testScaleneTriangle2(self):
-        self.assertEqual(classifyTriangle(1, 2, 3), 'Scalene', '1,2,3 should be Scalene triangle')
-
     def testScaleneTriangle3(self):
         self.assertEqual(classifyTriangle(100, 110, 112), 'Scalene', '100,110,112 should be Scalene triangle')
-
-    def testScaleneTriangle4(self):
-        self.assertEqual(classifyTriangle(10, 10, 12), 'Scalene', '10,10,12 should be Scalene triangle')
 
     def testIsocelesTriangle1(self):
         self.assertEqual(classifyTriangle(5, 5, 3),'Isoceles','5,5,3 should be a Isoceles triangle')
@@ -68,9 +50,6 @@ class TestTriangles(unittest.TestCase):
 
     def testNotATriangle2(self):
         self.assertEqual(classifyTriangle(1, 4, 5), 'NotATriangle', '1,4,5 NotATriangle')
-
-    def testNotATriangle3(self):
-        self.assertEqual(classifyTriangle(1, 0, 1), 'NotATriangle', '1,0,1 NotATriangle')
 
     def testNotATriangle4(self):
         self.assertEqual(classifyTriangle(1, 17, 5), 'NotATriangle', '1,17,5 NotATriangle')
